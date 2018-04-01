4136b93c93483a68b3930d9390df49838017aaa4
u-235 <nicegorov@yandex.ru>
2018-04-01 14:44:54 +0300

    исправления модуля датчика

--- src/sensor.c
+++ src/sensor.c
@@ -1,28 +1,19 @@
-/*****************************************************************************
- *
- *  micron 2 v 1.2.6
- *  Copyright (C) 2018  Nick Egorrov
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *****************************************************************************/
+/**
+ * \file
+ * \brief Реализация интерфейса датчика
+ * \date Создан 24.02.2018
+ * \author: Nick Egorrov
+ * \copyright GNU Public License 3
+ * \details
+ */
 
-#include "compiller.h"
+#include "compiler.h"
 #include "config.h"
-#include "power.h"
 #include "sensor.h"
-#include "alarm.h"
+#include "power.h"
+
+#define SCALE_MIN 3U
+#define PUMP_TICKS_INIT 6000UL
 
 /*************************************************************
  *      Private function prototype
@@ -38,30 +29,34 @@ static void CalculatePump(unsigned int voltage);
  * Run impulse sequence for charge a capacitor of sensor power supply.
  * \param ticks
  */
-static void RunCharge(unsigned int ticks);
+static void RunCharge(unsigned char ticks);
 
 /*************************************************************
  *      Variable in RAM
  *************************************************************/
 
+/* Флаг о срабатывании датчика, см. SensorIsHit() и _isr_ext1() */
 static unsigned char impHits;
-static unsigned int impCounter;
-static unsigned int impulse_massive[SENSOR_INDEX_MAX + 1];
+/* Массив зарегистрированных импульсов. Нулевой элемент используется как
+ * счётчик импульсов за текущую секунду. */
+static unsigned int impMassive[SENSOR_INDEX_MAX + 2];
 
+/* Накопленная доза за час, в импульсах датчика. */
 static unsigned long doseHour = 0;
+/* Накопленная доза за день, в импульсах датчика. */
 static unsigned long doseDay = 0;
+/* Общая накопленная доза, в мкР. */
 static unsigned long doseAll = 0;
 /* Масштабный коэффициент для вычисления относительных уровней радиации в
- * SensorGetLevel(). Равен самому большому значению в массиве.
- */
-static unsigned int scale = 6;
+ * SensorGetLevel(). Равен самому большому значению в массиве. */
+static unsigned int scale = SCALE_MIN;
 
 /* Длительность положительнного импульса накачки, рабочее значение. */
 static unsigned char pulseDuration;
 /* Количество импульсов принудительной накачки, рабочее значение. */
-static unsigned int ticksPeriodic;
+static unsigned char ticksPeriodic;
 /* Количество импульсов накачки при сработке датчика, рабочее значение. */
-static unsigned int ticksHit;
+static unsigned char ticksHit;
 
 /*************************************************************
  *      Variable in EEPROM
@@ -70,28 +65,52 @@ static unsigned int ticksHit;
 /* Длительность положительнного импульса накачки, настройка. */
 static eeprom unsigned char eePulseDuration = PUMP_PULSE_WIDTH_DEFAULT;
 /* Количество импульсов принудительной накачки, настройка. */
-static eeprom unsigned int eeTicksPeriodic = PUMP_TICKS_PERIODIK_DEFAULT;
+static eeprom unsigned char eeTicksPeriodic = PUMP_TICKS_PERIODIK_DEFAULT;
 /* Количество импульсов накачки при сработке датчика, настройка. */
-static eeprom unsigned int eeTicksHit = PUMP_TICKS_HIT_DEFAULT;
+static eeprom unsigned char eeTicksHit = PUMP_TICKS_HIT_DEFAULT;
 
 /*************************************************************
  *      Public function
  *************************************************************/
 
+/*
+ * Инициализация датчика. Параметры настройки извлекаются из EEPROM и
+ * запускается начальный цикл накачки.
+ */
 extern void InitSensor()
 {
+        unsigned char i;
+
+        _pin_off(IN_SENSOR);
+        _pin_off(OUT_PUMP_SWITCH);
+        _dir_in(IN_SENSOR);
+        _dir_out(OUT_PUMP_SWITCH);
         pulseDuration = _eemem_read8(&eePulseDuration);
-        ticksPeriodic = _eemem_read16(&eeTicksPeriodic);
-        ticksHit = _eemem_read16(&eeTicksHit);
+        ticksPeriodic = _eemem_read8(&eeTicksPeriodic);
+        ticksHit = _eemem_read8(&eeTicksHit);
         CalculatePump(GetVoltage());
-        RunCharge(6000);
+        for (i = PUMP_TICKS_INIT / 255; i > 0; i--) {
+                RunCharge(255);
+        }
+        RunCharge(PUMP_TICKS_INIT % 255);
+        _interrupt_enable(INT_EXT1);
 }
 
+/*
+ * Возвращает длительность импульса накачки преобразователя; задаётся в
+ * количестве циклов в RunCharge(), точное время неизвестно.
+ * \return Длительность импульса накачки преобразователя.
+ */
 extern unsigned char SensorGetPulseDuration()
 {
         return pulseDuration;
 }
 
+/*
+ * Увеличивает длительность импульса накачки преобразователя. При достижении
+ * #PUMP_PULSE_WIDTH_MAX сбрасывается в #PUMP_PULSE_WIDTH_MIN.
+ * \see SensorGetPulseDuration()
+ */
 extern void SensorIncPulseDuration()
 {
         unsigned char pulse = pulseDuration + 1;
@@ -102,41 +121,66 @@ extern void SensorIncPulseDuration()
         _eemem_write8(&eePulseDuration, pulse);
 }
 
+/*
+ * Возвращает количество импульсов накачки при запуске от таймера.
+ * \return Количество импульсов накачки.
+ */
 extern unsigned int SensorGetTicksPeriodik()
 {
         return ticksPeriodic;
 }
 
+/*
+ * Увеличивает количество импульсов накачки преобразователя при запуске от
+ * таймера. При достижении #PUMP_TICKS_PERIODIK_MAX сбрасывается в
+ * #PUMP_TICKS_PERIODIK_MIN.
+ * \see SensorGetTicksPeriodik()
+ */
 extern void SensorIncTicksPeriodik()
 {
-        unsigned int ticks = ticksPeriodic + 1;
+        unsigned char ticks = ticksPeriodic + 1;
         if (ticks > PUMP_TICKS_PERIODIK_MAX) {
                 ticks = PUMP_TICKS_PERIODIK_MIN;
         }
         ticksPeriodic = ticks;
-        _eemem_write16(&eeTicksPeriodic, ticks);
+        _eemem_write8(&eeTicksPeriodic, ticks);
 }
 
+/*
+ * Возвращает количество импульсов накачки при сработке датчика.
+ * \return Количество импульсов накачки.
+ */
 extern unsigned int SensorGetTicksHit()
 {
         return ticksHit;
 }
 
+/*
+ * Увеличивает количество импульсов накачки преобразователя при сработке
+ * датчика. При достижении #PUMP_TICKS_HIT_MAX сбрасывается в
+ * #PUMP_TICKS_HIT_MIN.
+ * \see SensorGetTicksHit()
+ */
 extern void SensorIncTicksHit()
 {
-        unsigned int ticks = ticksHit + 1;
+        unsigned char ticks = ticksHit + 1;
         if (ticks > PUMP_TICKS_HIT_MAX) {
                 ticks = PUMP_TICKS_HIT_MIN;
         }
         ticksHit = ticks;
-        _eemem_write16(&eeTicksHit, ticks);
+        _eemem_write8(&eeTicksHit, ticks);
 }
 
+/*
+ * Возвращает уровень радиации за указанный период в мкР/ч
+ * \param period Период в секундах, от 1 до #SENSOR_INDEX_MAX + 1
+ * \return Уровень радиации в мкР/ч.
+ */
 extern unsigned int SensorGetRadiation(unsigned char period)
 {
         unsigned int retval = 0, i;
 
-        if (--period == 0) {
+        if (period == 0) {
                 return 0;
         }
         if (period > SENSOR_INDEX_MAX) {
@@ -144,27 +188,44 @@ extern unsigned int SensorGetRadiation(unsigned char period)
         }
 
         for (i = 0; i <= period; i++) {
-                retval += impulse_massive[i];
+                retval += impMassive[i];
         }
         /* Не люблю магию, но тут 3600 - это количество секунд в часе. */
         return retval / (period + 1) * (3600 / SENSOR_SENSITIVITY);
 }
 
+/*
+ * Возвращает измеренную дозу за последний завершившийся час.
+ * \return Доза в мкР.
+ */
 extern unsigned long SensorGetDoseHour()
 {
         return doseHour / SENSOR_SENSITIVITY;
 }
 
+/*
+ * Возвращает измеренную дозу за последние завершившиеся сутки с момента
+ * сброса измерений функцией SensorClearDose().
+ * \return Доза в мкР.
+ */
 extern unsigned long SensorGetDoseDay()
 {
-        return doseDay + doseHour / SENSOR_SENSITIVITY;
+        return (doseDay + doseHour) / SENSOR_SENSITIVITY;
 }
 
+/*
+ * Возвращает измеренную дозу за всё время с момента сброса измерений функцией
+ * SensorClearDose().
+ * \return Доза в мкР.
+ */
 extern unsigned long SensorGetDoseAll()
 {
-        return doseAll + doseDay + doseHour / SENSOR_SENSITIVITY;
+        return doseAll + (doseDay + doseHour) / SENSOR_SENSITIVITY;
 }
 
+/*
+ * Сброс измерений дозы.
+ */
 extern void SensorClearDose()
 {
         doseHour = 0;
@@ -172,12 +233,21 @@ extern void SensorClearDose()
         doseAll = 0;
 }
 
+/*
+ * Возвращает относительный уровень радиации.
+ * \param index Индекс значения уровня. Диапазон от 0 до #SENSOR_INDEX_MAX.
+ * \return Относительный уровень радиации в диапазоне от 0 до
+ *       #DOSE_HISTOGRAM_MAX
+ */
 extern unsigned char SensorGetRelative(unsigned char index)
 {
-        return (unsigned int) impulse_massive[index] * DOSE_HISTOGRAM_SIZE
-                        / scale;
+        return (unsigned int) impMassive[++index] * DOSE_HISTOGRAM_MAX / scale;
 }
 
+/*
+ * Проверяет, была ли сработка датчика с момента предыдущего вызова функции.
+ * \return Ненулевое значение если была сработка датчика.
+ */
 extern unsigned char SensorIsHit()
 {
         unsigned char retval = impHits;
@@ -186,55 +256,50 @@ extern unsigned char SensorIsHit()
 }
 
 /* Количество импульсов принудительной накачки, вычисленное значение. */
-static unsigned int realTicksPeriodic;
+static unsigned char realTicksPeriodic;
 /* Количество импульсов накачки при сработке датчика, вычисленное значение. */
-static unsigned int realTicksHit;
+static unsigned char realTicksHit;
 
+/*
+ * Обновление данных датчика в зависимости от времени.
+ * \param event Набор флагов CLOCK_EVENT_xx см. "clock.h"
+ */
 extern void SensorClockEvent(unsigned char event)
 {
-        int i;
+        unsigned char i;
+        unsigned int old;
 
         if (event & CLOCK_EVENT_MINUTE) {
                 CalculatePump(GetVoltage());
         }
 
         if (event & CLOCK_EVENT_SECOND) {
-                scale = 6;
+                scale = SCALE_MIN;
                 /* смещение массива фона */
-                for (i = SENSOR_INDEX_MAX; i >= 0; i--) {
-                        unsigned int old;
-                        if (i == 0) {
-                                old = impCounter;
-                        } else {
-                                old = impulse_massive[i - 1];
-                        }
+                for (i = SENSOR_INDEX_MAX; i > 0; i--) {
+                        _cli();
+                        old = impMassive[i - 1];
+                        _sei();
                         if (old > scale) {
                                 scale = old;
                         }
-                        impulse_massive[i] = old;
+                        impMassive[i] = old;
                 }
-                impCounter = 0;
-
-                GICR = 0x00;
-                /* TODO remove led flash */
-                led_refresh(0);
+                _cli();
+                impMassive[0] = 0;
+                _sei();
                 RunCharge(realTicksPeriodic);
-                delay_us(25);  //25
-                led_refresh(1);
-                GICR = 0xC0;
         }
 
         if (event & CLOCK_EVENT_HOUR) {
-                /* TODO Очень странное место. По идее, */
-                // 60сек * 60мин / 36 = 100. так проще для МК
-                // вот здесь я ввел дополнительную переменную для почасовой дозы.
-                // наверное, таким образом баг и починился :D
-                doseDay += doseHour / 100;
+                _cli();
+                doseDay += doseHour;
                 doseHour = 0;
+                _sei();
         }
 
         if (event & CLOCK_EVENT_DAY) {
-                doseAll += doseDay;
+                doseAll += doseDay / SENSOR_SENSITIVITY;
                 doseDay = 0;
         }
 }
@@ -245,13 +310,10 @@ extern void SensorClockEvent(unsigned char event)
 
 _isr_ext1(void)
 {
-        /* TODO Оно тут надо? */
-        _pin_off(OUT_BEEPER);
         RunCharge(realTicksHit);
         impHits = 1;
-        impCounter++;
+        impMassive[0]++;
         doseHour++;
-        _wdr();
         /* TODO Нужно улучшить - отслеживать состояние на входе INT1 */
         delay_us(1);
 }
@@ -263,19 +325,20 @@ _isr_ext1(void)
 static void CalculatePump(unsigned int voltage)
 {
 #ifdef POWER_LION
-        realTicksHit = (POWER_VOLTAGE_HIGH - SWITCH_DROP_VOLTAGE) * ticksHit
+        realTicksHit = (unsigned int) (POWER_VOLTAGE_HIGH - SWITCH_DROP_VOLTAGE)
+                        * ticksHit / (voltage - SWITCH_DROP_VOLTAGE);
+        realTicksPeriodic = (unsigned int) (POWER_VOLTAGE_HIGH
+                        - SWITCH_DROP_VOLTAGE) * ticksPeriodic
                         / (voltage - SWITCH_DROP_VOLTAGE);
-        realTicksPeriodic = (POWER_VOLTAGE_HIGH - SWITCH_DROP_VOLTAGE)
-                        * ticksPeriodic / (voltage - SWITCH_DROP_VOLTAGE);
 #else // not lithium
         realTicksHit = ticksHit;
         realTicksPeriodic = ticksPeriodic;
 #endif
 }
 
-static void RunCharge(unsigned int ticks)
+static void RunCharge(unsigned char ticks)
 {
-        unsigned int i, j;
+        unsigned char i, j;
 
         for (i = 0; i < ticks; i++) {
                 _pin_on(OUT_PUMP_SWITCH);
--- src/sensor.h
+++ src/sensor.h
@@ -1,30 +1,10 @@
-/*****************************************************************************
- *
- *  micron 2 v 1.2.6
- *  Copyright (C) 2018  Nick Egorrov
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *****************************************************************************/
-
 /**
  * \file
- * \brief
+ * \brief Интерфейс датчика
+ * \date Создан 24.02.2018
+ * \author: Nick Egorrov
+ * \copyright GNU Public License 3
  * \details
- *
- * \date created on: 24.02.2018
- * \author: nick
  */
 
 #ifndef SRC_SENSOR_H_
@@ -38,6 +18,10 @@ extern "C" {
 
 #define SENSOR_INDEX_MAX 35
 
+/**
+ * Инициализация датчика. Параметры настройки извлекаются из EEPROM и
+ * запускается начальный цикл накачки.
+ */
 extern void InitSensor();
 
 /**
@@ -47,16 +31,16 @@ extern void InitSensor();
 extern void SensorClockEvent(unsigned char event);
 
 /**
- *
+ * Проверяет, была ли сработка датчика с момента предыдущего вызова функции.
  * \return Ненулевое значение если была сработка датчика.
  */
 extern unsigned char SensorIsHit();
 
 /**
  * Возвращает относительный уровень радиации.
- * \param index Индекс значения уровня. Диаппазон от 0 до #SENSOR_INDEX_MAX.
- * \return Относительный уровень радиации в диаппазоне от 0 до
- *       #DOSE_HISTOGRAM_SIZE
+ * \param index Индекс значения уровня. Диапазон от 0 до #SENSOR_INDEX_MAX.
+ * \return Относительный уровень радиации в диапазоне от 0 до
+ *       #DOSE_HISTOGRAM_MAX
  */
 extern unsigned char SensorGetRelative(unsigned char index);
 
@@ -66,33 +50,80 @@ extern unsigned char SensorGetRelative(unsigned char index);
 
 /**
  * Возвращает уровень радиации за указанный период в мкР/ч
- * @param period Период в секундах, от 1 до SENSOR_INDEX_MAX + 1
- * @return Уровень радиации в мкР/ч
+ * \param period Период в секундах, от 1 до #SENSOR_INDEX_MAX + 1
+ * \return Уровень радиации в мкР/ч.
  */
 extern unsigned int SensorGetRadiation(unsigned char period);
 
+/**
+ * Возвращает измеренную дозу за последний завершившийся час.
+ * \return Доза в мкР.
+ */
 extern unsigned long SensorGetDoseHour();
 
+/**
+ * Возвращает измеренную дозу за последние завершившиеся сутки с момента
+ * сброса измерений функцией SensorClearDose().
+ * \return Доза в мкР.
+ */
 extern unsigned long SensorGetDoseDay();
 
+/**
+ * Возвращает измеренную дозу за всё время с момента сброса измерений функцией
+ * SensorClearDose().
+ * \return Доза в мкР.
+ */
 extern unsigned long SensorGetDoseAll();
 
+/**
+ * Сброс измерений дозы.
+ */
 extern void SensorClearDose();
 
 /*************************************************************
  *      Setup section
  *************************************************************/
 
+/**
+ * Возвращает длительность импульса накачки преобразователя; задаётся в
+ * количестве циклов в RunCharge(), точное время неизвестно.
+ * \return Длительность импульса накачки преобразователя.
+ */
 extern unsigned char SensorGetPulseDuration();
 
+/**
+ * Увеличивает длительность импульса накачки преобразователя. При достижении
+ * #PUMP_PULSE_WIDTH_MAX сбрасывается в #PUMP_PULSE_WIDTH_MIN.
+ * \see SensorGetPulseDuration()
+ */
 extern void SensorIncPulseDuration();
 
+/**
+ * Возвращает количество импульсов накачки при запуске от таймера.
+ * \return Количество импульсов накачки.
+ */
 extern unsigned int SensorGetTicksPeriodik();
 
+/**
+ * Увеличивает количество импульсов накачки преобразователя при запуске от
+ * таймера. При достижении #PUMP_TICKS_PERIODIK_MAX сбрасывается в
+ * #PUMP_TICKS_PERIODIK_MIN.
+ * \see SensorGetTicksPeriodik()
+ */
 extern void SensorIncTicksPeriodik();
 
+/**
+ * Возвращает количество импульсов накачки при сработке датчика.
+ * \return Количество импульсов накачки.
+ */
 extern unsigned int SensorGetTicksHit();
 
+/**
+ * Увеличивает количество импульсов накачки преобразователя при сработке
+ * датчика. При достижении #PUMP_TICKS_HIT_MAX сбрасывается в
+ * #PUMP_TICKS_HIT_MIN.
+ * \see SensorGetTicksHit()
+ */
 extern void SensorIncTicksHit();
 
 #ifdef _cplusplus
