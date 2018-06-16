ae95ba4c1684a7ad3aef7f9cef9605902dca0089
u-235 <nicegorov@yandex.ru>
2018-06-16 12:42:56 +0300

    main.c -> app.c

---
+++ src/app.h
@@ -0,0 +1,24 @@
+/**
+ * \file
+ * \brief
+ * \details
+ *
+ * \author Nick Egorrov
+ * \date created on: 16.06.2018
+ * \copyright GNU Public License 3
+ */
+
+#ifndef SRC_APP_H_
+#define SRC_APP_H_
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef _cplusplus
+}
+#endif
+
+#endif /* SRC_APP_H_ */
