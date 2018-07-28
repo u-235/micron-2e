6827ee0dd842b62d38b739346ec98e12041e62ae
u-235 <nicegorov@yandex.ru>
2018-07-28 16:34:21 +0300

    Переделка главного цикла
    Главный цикл изменен так, что бы при старте программы устройство
    переходило во включенный режим с показом экрана приветсвия именно из
    главного цикла. Это позволяет включать и выключать устройство не
    прерывая работы программы.

--- src/app.c
+++ src/app.c
@@ -17,24 +17,56 @@
 #include "sensor.h"
 #include "user.h"
 
+/**
+ * Задержка при включении, секунд.
+ */
+#define APP_DELAY_ON    7
+/**
+ * Задержка при выключении, секунд.
+ */
+#define APP_DELAY_OFF   3
+
+/**
+ * Максимальное значение времени задержки перехода в режим энергосбережения.
+ * Это значение не может быть больше чем 255*APP_SAVE_DELAY_STEP.
+ */
+#define APP_SAVE_DELAY_MAX            600U
+
+/**
+ * Значение времени задержки перехода в режим энергосбережения по умолчанию.
+ */
+#define APP_SAVE_DELAY_DEFAULT        30U
+
+/**
+ * Шаг отсчёта времени задержки перехода в режим энергосбережения. Эта величина
+ * используется для уменьшения размера переменной, отвечающей за отсчёт
+ * задержки.
+ */
+#define APP_SAVE_DELAY_STEP           10U
+
 /*************************************************************
  *      Private function prototype
  *************************************************************/
 
-static void AppInit();
+static void AppOn();
+static void AppOff();
 static void AppClockEvent(unsigned char event);
-static void AppStartSaveTimer();
+static void AppStartSaveTimer(unsigned char key);
+static void AppToggle(unsigned char key);
 
 /*************************************************************
  *      Variable in RAM
  *************************************************************/
 
 /* Текущий режим работы устройства. См. AppSetMode(). */
-static unsigned char appMode;
+static unsigned char appMode = APP_MODE_OFF;
 /* Время задержки перехода в режим энергосбережения. */
-static unsigned char saveDelay;
+static unsigned char saveDelay = 0;
 /* Счётчик перехода в режим энергосбережения. */
-static unsigned int saveTimer;
+static unsigned int saveTimer = 0;
+/* Счётчик задержки переключения включен-выключен. */
+static unsigned char toggleTimer = 0;
+static unsigned char toggleMode = APP_MODE_OFF;
 
 /*************************************************************
  *      Variable in EEPROM
@@ -52,48 +84,50 @@ static eeprom unsigned char eeSaveDelay = APP_SAVE_DELAY_DEFAULT
 
 void main(void)
 {
-        WDTCR = 0x1F;
-        WDTCR = 0x0F;
-        _sei();
-        AppInit();
-        AppSetMode(APP_MODE_ON);
-        ScreenShow(SCREEN_VIEW_INTRO);
-        ScreenDraw();
-        delay_ms(2000);
+        unsigned char event, key;
+        struct {
+                char sensorError :1;
+                char sensorAlarm :1;
+                char powerBad :1;
+        } flags = {
+                0, 0, 0
+        };
+
+        /*
+         * Подсовываем нажатие кнопки "питание" что бы запустить устройство
+         * при подаче напряжения. Именно поэтому опрос кнопок происходит в
+         * конце бесконечного цикла после выхода из спячки.
+         * Возможно стоит написать проверку, не было ли срабатывания сторожевого
+         * таймера.
+         */
+        key = USER_KEY_POWER_DOWN;
 
         while (1) {
-                unsigned char event, key;
-                struct {
-                        char sensorBad :1;
-                        char powerBad :1;
-                        char sensorError :1;
-                } flags = {
-                        0, 0, 0
-                };
 
-                /* Получение и обработка событий программного таймера. */
-                event = ClockGetEvent();
-                AppClockEvent(event);
-                SensorClockEvent(event);
-                PowerClockEvent(event);
-                UserClockEvent(event);
-                ScreenClockEvent(event);
+                /* Обработка событий от кнопок. */
+                AppToggle(key);
+                AppStartSaveTimer(key);
+                ScreenHandleKey(key);
+
+                if (SensorIsHit()){
+                        /*TODO Сделать реализацию щелчка при сработке датчика.*/
+                }
 
                 /* Проверка превышения максимальной радиации. */
                 if (SensorIsAlarm()) {
-                        if (flags.sensorBad == 0) {
-                                flags.sensorBad = 1;
+                        if (flags.sensorAlarm == 0) {
+                                flags.sensorAlarm = 1;
                                 UserAlert(USER_ALERT_DOSE);
                                 ScreenShow(SCREEN_VIEW_ALERT_DOSE);
                         }
                 } else {
-                        flags.sensorBad = 0;
+                        flags.sensorAlarm = 0;
                         ScreenHide(SCREEN_VIEW_ALERT_DOSE);
                 }
 
                 /* Проверка работоспособности датчика. */
                 if (SensorError()) {
-                        if (flags.sensorBad == 0) {
+                        if (flags.sensorError == 0) {
                                 flags.sensorError = 1;
                                 UserAlert(USER_ALERT_SENSOR);
                         }
@@ -113,24 +147,20 @@ void main(void)
                         ScreenHide(SCREEN_VIEW_ALERT_POWER);
                 }
 
-                /* Получение и обработка событий от кнопок. */
-                key = UserGetKey();
-                if (key != 0) {
-                        AppStartSaveTimer();
-                        ScreenHandleKey(key);
-                }
-
-                if (key == USER_KEY_POWER_DOWN) {
-                        ScreenShow(SCREEN_VIEW_BAY);
-                }
-
+                /* Обновление экрана и спячка. */
                 ScreenDraw();
-
-                if (key == USER_KEY_POWER_DOWN) {
-                        delay_ms(2000);
-                        AppSetMode(APP_MODE_OFF);
-                }
                 _sleep();
+
+                /* Получение и обработка событий программного таймера. */
+                event = ClockGetEvent();
+                AppClockEvent(event);
+                SensorClockEvent(event);
+                PowerClockEvent(event);
+                UserClockEvent(event);
+                ScreenClockEvent(event);
+
+                /* Получение событий от кнопок. */
+                key = UserGetKey();
         }
 }
 
@@ -149,6 +179,7 @@ extern void AppSetMode(unsigned char mode)
 
         switch (mode) {
         case APP_MODE_ON:
+                AppOn();
                 PowerOn();
                 ClockOn();
                 SensorOn();
@@ -166,21 +197,13 @@ extern void AppSetMode(unsigned char mode)
                 break;
         case APP_MODE_OFF:
         default:
+                AppOff();
                 PowerOff();
                 ClockOff();
                 SensorOff();
                 ScreenOff();
                 UserOff();
                 LcdOff();
-                GICR = 0x40;
-                MCUCR = 0xA0;
-                DDRB = 0x00;
-                DDRC = 0x00;
-                DDRD = 0x00;
-                PORTB = 0x00;
-                PORTC = 0x00;
-                PORTD = 0x04;
-                TCCR0 = 0x00;
         }
 }
 
@@ -231,33 +254,94 @@ extern void AppIncSaveTime()
 /*
  * Инициализация модуля.
  */
-static void AppInit()
+static void AppOn()
 {
+        WDTCR = 0x1F;
+        WDTCR = 0x0F;
+        _sei();
         saveDelay = _eemem_read8(&eeSaveDelay);
 }
 
-/*
- * Обновление внутреннего состояния модуля.
- * \param event Набор флагов CLOCK_EVENT_xx см. "clock.h"
+static void AppOff()
+{
+        GICR = 0x40;
+        MCUCR = 0xA0;
+        TCCR0 = 0x00;
+}
+
+/**
+ * \brief Обновление внутреннего состояния модуля.
+ * \details Обновления происходят каждую секунду. По окончанию счёта таймера
+ *  saveTimer() происходит переключение в экономный режим. По окончанию счёта
+ *  таймера toggleTimer() происходит смена режима включено/выключено.
+ * \param event Набор флагов CLOCK_EVENT_xx см. clock.h
  */
 static void AppClockEvent(unsigned char event)
 {
-        if (appMode == APP_MODE_OFF || (event & CLOCK_EVENT_SECOND) == 0) {
+        if ((event & CLOCK_EVENT_SECOND) == 0) {
                 return;
         }
-        if (saveTimer != 0) {
+
+        if (saveTimer != 0 && appMode != APP_MODE_OFF) {
                 saveTimer--;
                 if (saveTimer == 0) {
                         AppSetMode(APP_MODE_SAVE);
                 }
         }
+
+        if (toggleTimer != 0) {
+                toggleTimer--;
+                if (toggleTimer == 0) {
+                        AppSetMode(toggleMode);
+                        ScreenHide(SCREEN_VIEW_INTRO);
+                        ScreenHide(SCREEN_VIEW_BAY);
+                }
+        }
+}
+
+/**
+ * \brief Подготовка к переключению режима работы устройства.
+ * \details Если нажата кнопка #USER_KEY_POWER_DOWN и процесс подготовки ещё
+ *  не запущен, то подготовка запускается. Механизм переключения использует
+ *  таймер toggleTimer() для задержек.
+ *
+ *  Если устройство отключено, сразу же устанавливается режим #APP_MODE_ON,
+ *  запускается таймер и показывается приветствие.
+ *
+ *  Если устройство включено, то запускается таймер и показывается экран
+ *  выключения. Устройство выключается в AppClockEvent() по окончании работы
+ *  таймера.
+ *
+ * \param key Код нажатой кнопки.
+ */
+static void AppToggle(unsigned char key)
+{
+        if (toggleTimer != 0 || key != USER_KEY_POWER_DOWN) {
+                return;
+        }
+
+        if (appMode == APP_MODE_OFF) {
+                AppSetMode(APP_MODE_ON);
+                toggleMode = APP_MODE_ON;
+                toggleTimer = APP_DELAY_ON;
+                ScreenShow(SCREEN_VIEW_INTRO);
+        } else {
+                toggleMode = APP_MODE_OFF;
+                toggleTimer = APP_DELAY_OFF;
+                ScreenShow(SCREEN_VIEW_BAY);
+        }
+
 }
 
 /*
  * Перезапуск счетчика задержки перехода в режим пониженного потребления
  * питания.
  */
-static void AppStartSaveTimer()
+static void AppStartSaveTimer(unsigned char key)
 {
+        if (key == 0) {
+                return;
+        }
+
         saveTimer = saveDelay * APP_SAVE_DELAY_STEP;
 }
